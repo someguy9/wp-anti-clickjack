f60aac945bea30f034bcd2c15f88b89b162e5196
Andy Feliciotti <andy@drawne.com>
2017-05-16 13:54:08 -0600

    Add to login page

--- wp-anti-clickjack.php
+++ wp-anti-clickjack.php
@@ -18,6 +18,7 @@ class wp_anticlickjack {
     {
 			add_action('wp_head', array($this,'include_anticlickjack_script'));
       add_action('admin_head', array($this,'include_anticlickjack_script'));
+      add_action('login_head', array($this,'include_anticlickjack_script'));
     }
 
     public function include_anticlickjack_script() {
